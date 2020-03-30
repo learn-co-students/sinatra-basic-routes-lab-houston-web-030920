3ac66300cb209c3412eee32b99b655d64bee1a52
Rooted1 <ruthobey@gmail.com>
2020-03-29 19:40:01 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World"
+  end
+
+  get '/name' do
+    "My name is Ruth"
+  end
+
+  get '/hometown' do
+    "My hometown is Manvel, Texas"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Mama Africa by Yemi Alade"
+  end
 end
